d2a6c2b9690b3ad8eea53a47a25618be7c1e538e
nurulamin092 <mnurulamin092@gmial.com>
2022-02-13 08:48:58 +0600

    case pluse button is ok

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-document.getElementById('case-pluse')
-    .addEventListener('click', function () {
-        console.log("bismillah");
-    })
+document.getElementById('case-pluse').addEventListener('click', function () {
+    const caseInputNumber = document.getElementById('case-number');
+    const inputNumberText = caseInputNumber.value;
+    const inputNumber = parseInt(inputNumberText);
+    caseInputNumber.value = inputNumber + 1;
+});
