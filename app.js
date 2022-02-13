052bfed35a882e00afd0c5266f7aa6cbd0010fea
nurulamin092 <mnurulamin092@gmial.com>
2022-02-13 17:07:40 +0600

    code repector

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 
-function updateProductNumber(product, price, isIncreasing) {
-    const productInput = document.getElementById(product + '-number');
-    let productNumber = productInput.value;
-    if (isIncreasing == true) {
-        productNumber = parseInt(productNumber) + 1;
+function updateNumber(product, price, isIncrease) {
+    const caseInputNumber = document.getElementById(product + '-number');
+    let productNumber = parseInt(caseInputNumber.value);
+    if (isIncrease == true) {
+        productNumber = productNumber + 1;
     }
-
     else if (productNumber > 0) {
-        productNumber = parseInt(productNumber) - 1;
+        productNumber = productNumber - 1;
     }
-    productInput.value = productNumber;
+    caseInputNumber.value = productNumber;
 
-    //Update total
-    const productTotal = document.getElementById(product + '-total');
-    productTotal.innerText = productNumber * price;
+    const caseTotal = document.getElementById(product + '-total');
+    caseTotal.innerText = productNumber * price;
+
 }
-document.getElementById('phone-pluse').
-    addEventListener('click', function () {
-        updateProductNumber('phone', 1219, true);
-    });
+
+document.getElementById('phone-pluse').addEventListener('click', function () {
+    updateNumber('phone', 1219, true);
+});
 document.getElementById('phone-minus').addEventListener('click', function () {
-    updateProductNumber('phone', 1219, false);
+    updateNumber('phone', 1219, false)
 });
 
-
-//case pluse
 document.getElementById('case-pluse').addEventListener('click', function () {
-    updateProductNumber('case', 59, true);
+    updateNumber('case', 59, true)
 });
-//case minus
-document.getElementById('case-minus').addEventListener('click', function () {
-    updateProductNumber('case', 59, false);
 
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateNumber('case', 59, false)
 });
