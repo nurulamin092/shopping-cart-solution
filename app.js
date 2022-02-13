c470d4ead04c15b922b8dbc97a886c8135f36986
nurulamin092 <mnurulamin092@gmial.com>
2022-02-13 09:09:15 +0600

    code recap add function

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-document.getElementById('case-pluse').addEventListener('click', function () {
-    const caseInputNumber = document.getElementById('case-number');
-    const inputNumberText = caseInputNumber.value;
-    const inputNumber = parseInt(inputNumberText);
-    caseInputNumber.value = inputNumber + 1;
-});
 
-document.getElementById('case-minus').addEventListener('click', function () {
+function updateInputNumber(isIncreasing) {
     const caseInputNumber = document.getElementById('case-number');
     const caseInputNumberText = caseInputNumber.value;
     const inputNumber = parseInt(caseInputNumberText);
-    if (inputNumber > 0) {
+    if (isIncreasing == true) {
+        caseInputNumber.value = inputNumber + 1;
+    }
+
+    else if (inputNumber > 0) {
         caseInputNumber.value = inputNumber - 1;
     }
+
+}
+
+
+document.getElementById('case-pluse').addEventListener('click', function () {
+    updateInputNumber(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateInputNumber(false);
+
 });
